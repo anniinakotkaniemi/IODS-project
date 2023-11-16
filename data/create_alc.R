cbb877947e248eed5a2dcb45b5907ff2177ef79c
Kotkaniemi <anniipen@ad.helsinki.fi>
2023-11-16 15:07:47 +0200

    initial commit

---
+++ data/create_alc.R
@@ -0,0 +1,2 @@
+# Anniina Kotkaniemi, 16.11.2023. Data wrangling exercise of Assignment 3 in Introduction to Open Data Science Course
+# the data retreived from http://www.archive.ics.uci.edu/dataset/320/student+performance
